cb4d1e3990d7f7bf5757d2a2d473defd999f8b55
Rafael Santana <rafa03santana@gmail.com>
2026-02-28 20:58:34 -0300

    Adicionar arquivos de projeto.

---
+++ 1 - Two Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        //Dicionario para mapear valor e o indice
+        Dictionary<int, int> numToIndex = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            // Calcular o complemento necessário para atingir o alvo
+            int complement = target - nums[i];
+            // Verificar se o complemento já está no dicionário
+            if (numToIndex.ContainsKey(complement))
+            {
+                // Se encontrado, retornar os índices
+                return new int[] { numToIndex[complement], i };
+            }
+            // Caso contrário, adicionar o número atual e seu índice ao dicionário
+            numToIndex[nums[i]] = i;
+        }
+
+        throw new ArgumentException("Nenhuma solução encontrada");
+    }
+}
