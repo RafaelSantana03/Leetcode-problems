585517a877b133c59b0cccb4a2889f7f7df6ac30
Rafael Santana <rafa03santana@gmail.com>
2026-02-28 21:53:54 -0300

    Add Two Numbers - Solution

--- 2 - Add Two Numbers/Program.cs
+++ 2 - Add Two Numbers/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Solution
+﻿using System;
+
+public class Solution
 {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
@@ -34,9 +36,47 @@ public class ListNode
 {
     public int val;
     public ListNode next;
-    public ListNode(int val = 0, ListNode next = null)
+    public ListNode(int val = 0, ListNode? next = null)
     {
         this.val = val;
         this.next = next;
+    }
+}
+
+internal static class Program
+{
+    private static void Main()
+    {
+        // Exemplo de uso: l1 = [2,4,3], l2 = [5,6,4] -> resultado [7,0,8]
+        var l1 = Build(new int[] { 2, 4, 3 });
+        var l2 = Build(new int[] { 5, 6, 4 });
+
+        var resultado = new Solution().AddTwoNumbers(l1, l2);
+
+        Console.Write("Resultado: ");
+        PrintList(resultado);
+    }
+
+    private static ListNode Build(int[] digits)
+    {
+        var cabeca = new ListNode(0);
+        var atual = cabeca;
+        foreach (var d in digits)
+        {
+            atual.next = new ListNode(d);
+            atual = atual.next;
+        }
+        return cabeca.next!;
+    }
+
+    private static void PrintList(ListNode node)
+    {
+        while (node != null)
+        {
+            Console.Write(node.val);
+            if (node.next != null) Console.Write(" -> ");
+            node = node.next;
+        }
+        Console.WriteLine();
     }
 }
