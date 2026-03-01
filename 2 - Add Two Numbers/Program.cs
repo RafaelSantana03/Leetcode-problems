82502452a0b1e2b88a2f0a48564c99cbc534da40
Rafael Santana <rafa03santana@gmail.com>
2026-02-28 21:04:21 -0300

    'Add Two Numbers' - Solution

---
+++ 2 - Add Two Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode cabecaFicticia = new ListNode(0);
+        ListNode atual = cabecaFicticia;
+        int vaiUm = 0;
+
+        while (l1 != null || l2 != null || vaiUm != 0)
+        {
+            int soma = vaiUm;
+            if (l1 != null)
+            {
+                soma += l1.val; // Use .val, não .valor
+                l1 = l1.next;   // Use .next, não .proximo
+            }
+            if (l2 != null)
+            {
+                soma += l2.val;
+                l2 = l2.next;
+            }
+
+            vaiUm = soma / 10;
+            int digito = soma % 10;
+            atual.next = new ListNode(digito);
+            atual = atual.next;
+        }
+
+        return cabecaFicticia.next;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
