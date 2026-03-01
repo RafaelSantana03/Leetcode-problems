836f6d36b5cac582558286fb61f87572d00cbc30
Rafael Santana <rafa03santana@gmail.com>
2026-02-28 21:53:18 -0300

    Palindrome Number - Solution

---
+++ 9 - Palindrome Number/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+
+        int valorOriginal = x;
+        int valorRevertido = 0;
+
+        while (x > 0)
+        {
+            int ultimoDigito = x % 10;
+            valorRevertido = valorRevertido * 10 + ultimoDigito;
+            x /= 10;
+        }
+        if (valorOriginal == valorRevertido)
+        {
+            return true;
+        }
+        return false;
+
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int teste = 1221;
+        var resultado = new Solution().IsPalindrome(teste);
+        Console.WriteLine(resultado);
+    }
+}
