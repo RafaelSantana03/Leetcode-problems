efa22ac01003ef94680a09511b2ca960762fa7a5
Rafael Santana <rafa03santana@gmail.com>
2026-03-01 21:52:28 -0300

    feat: adiciona solução do problema 0013 - Roman To Integer

---
+++ 0013-Roman to Integer/Program.cs
@@ -0,0 +1,38 @@
+﻿
+var valor = "LVIII";
+var resultado = new Solution().RomanToInt(valor);
+Console.WriteLine(resultado);
+
+
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> romanMap = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        int total = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int currentValue = romanMap[s[i]];
+            int nextValue = (i + 1 < s.Length) ? romanMap[s[i + 1]] : 0;
+            if (currentValue < nextValue)
+            {
+                total -= currentValue; // Subtrair se o valor atual for menor que o próximo
+            }
+            else
+            {
+                total += currentValue; // Adicionar caso contrário
+            }
+        }
+        return total;
+    }
+}
